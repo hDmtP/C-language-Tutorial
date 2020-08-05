0eb7062939299cdf847dc1b2f854f3d6635c6198
Pritam Dhara <pritamdhara914@gmail.com>
2020-08-05 22:32:58 +0530

    decimal to binary

---
+++ C_Programms/decimal-to-binary.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    int rem;
+    int a[10];
+    int i=0;
+    printf("Enter any decimal digit:\n", n);
+    scanf("%d", &n);
+    while (n)
+    {
+        rem=n%2;
+        n=n/2;
+        a[i]=rem;
+        i++;
+    }
+    for (int j = i-1; j>=0; j--)
+    {
+        printf("%d", a[j]);
+    }
+    
+    
+    return 0;
+}
