eebec495efc5e4820898d3b0106e26d32b9d455c
kutti250 <72146635+kutti250@users.noreply.github.com>
2020-09-30 19:52:05 +0530

    made the code realistic

--- 6-practice.c
+++ 6-practice.c
@@ -3,7 +3,7 @@
 int main(){
     float celcius;
     float farenhite;
-    printf("Temp in celcius is \n");
+    printf("Enter the Temp in celcius: \n");
     scanf("%f", &celcius);
 
     printf("Temp in Farenhite is %f", (celcius * 9/5) + 32);
